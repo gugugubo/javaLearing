3fe11775224764f0f87e7927da4d26ab2f6c5d7e
guchunbo <1195550354@qq.com>
2020-03-05 16:08:32 +0800

    搞了一天的字符串常量池

--- Construct/jvm/src/main/java/com/gcb/jvm/bytecodetest/MyTest1.java
+++ Construct/jvm/src/main/java/com/gcb/jvm/bytecodetest/MyTest1.java
@@ -17,4 +17,30 @@ public class MyTest1 {
         this.a = a;
     }
 
+    public static void main(String[] args) {
+        String test = "javalanguagespecification";
+        String str = "java";
+        String str2 = "language";
+        String str3 = "specification";
+        System.out.println(test == str + str2 + str3);
+        System.out.println(test == "java" + "language" + "specification");
+    }
+//public static void main(String[] args) {
+//    String s1 = "hello";
+//    String s2 = "hello";
+//    String s3 = "he" + "llo";
+//    String s4 = "hel" + new String("lo");
+//    String s5 = new String("hello");
+//    String s6 = s5.intern();
+//    String s7 = "h";
+//    String s8 = "ello";
+//    String s9 = s7 + s8;
+//    System.out.println(s1==s2);//true
+//    System.out.println(s1==s3);//true
+//    System.out.println(s1==s4);//false
+//    System.out.println(s1==s9);//false
+//    System.out.println(s4==s5);//false
+//    System.out.println(s1==s6);//true
+//}
+
 }
--- Construct/jvm/src/main/java/com/gcb/jvm/gc/MyTest3.java
+++ Construct/jvm/src/main/java/com/gcb/jvm/gc/MyTest3.java
@@ -2,12 +2,29 @@ package com.gcb.jvm.gc;
 
 public class MyTest3 {
 
+//    public static void main(String[] args) {
+//        int size = 1024 * 1024;
+//        byte[] myAlloc1 = new byte[2 * size];
+//        byte[] myAlloc2 = new byte[2 * size];
+//        byte[] myAlloc3 = new byte[2 * size];
+//        byte[] myAlloc4 = new byte[2 * size];
+//        System.out.println("完成了");
+//    }
+
     public static void main(String[] args) {
-        int size = 1024 * 1024;
-        byte[] myAlloc1 = new byte[2 * size];
-        byte[] myAlloc2 = new byte[2 * size];
-        byte[] myAlloc3 = new byte[2 * size];
-        byte[] myAlloc4 = new byte[2 * size];
-        System.out.println("完成了");
+        String s1 = new String("hello");
+        String intern1 = s1.intern();
+        String s2 = "hello";
+        System.out.println(s1 == s2);
+        String s3 = new String("hello") + new String("hello");
+        String intern3 = s3.intern();
+        String s4 = "hellohello";
+        System.out.println(s3 == s4);
     }
+    /**
+     * 　　在jdk1.6下运行的结果为：
+     * 　　false,false
+     * 　　在jdk1.7,1.8下运行的结果为：
+     * 　　false,true
+     */
 }
