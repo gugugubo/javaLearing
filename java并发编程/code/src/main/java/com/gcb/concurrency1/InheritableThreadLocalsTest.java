c01ef551c4850a402d0a52a30d65b8033f2a8029
guchunbo <1195550354@qq.com>
2020-05-02 20:34:56 +0800

    更新

--- java并发编程/code/src/main/java/com/gcb/concurrency1/InheritableThreadLocalsTest.java
+++ java并发编程/code/src/main/java/com/gcb/concurrency1/InheritableThreadLocalsTest.java
@@ -1,4 +1,20 @@
 package com.gcb.concurrency1;
 
+
+/**
+ * InheritableThreadLocal类的使用测试
+ */
 public class InheritableThreadLocalsTest {
+
+    public static InheritableThreadLocal<String> threadLocal = new InheritableThreadLocal<String>();
+
+    public static void main(String[] args) {
+        threadLocal.set("I am main thread");
+
+        new Thread(()->{
+            System.out.println("sub Thread " + threadLocal.get());
+        }).start();
+
+        System.out.println("main Thread " + threadLocal.get());
+    }
 }
