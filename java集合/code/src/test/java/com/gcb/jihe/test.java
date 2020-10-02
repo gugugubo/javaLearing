7af9d7330d6c671598499c104bac0bc23a6122fd
guchunbo <1195550354@qq.com>
2020-10-02 21:24:22 +0800

    feat: hashmap
    hashmap的一些重要的方法和属性

---
+++ java集合/code/src/test/java/com/gcb/jihe/test.java
@@ -0,0 +1,20 @@
+package com.gcb.jihe;
+
+import org.junit.Test;
+
+/**
+ * @Author 古春波
+ * @Description TODO
+ * @Date 2020/9/29 16:47
+ * @Version 1.0
+ **/
+public class test {
+    
+    @Test
+    public void test(){
+        
+        String text = "<p><img class=\"wscnph\" src=\"http://macro-oss.oss-cn-shenzhen.aliyuncs.com/mall/images/20180604/155x54.bmp\" /><img class=\"wscnph\" src=\"http://macro-oss.oss-cn-shenzhen.aliyuncs.com/mall/images/20180604/APP登录bg1080.jpg\" width=\"500\" height=\"500\" /><img class=\"wscnph\" src=\"http://macro-oss.oss-cn-shenzhen.aliyuncs.com/mall/images/20180604/APP登录界面.jpg\" width=\"500\" height=\"500\" /></p>";
+        
+    
+    }
+}
